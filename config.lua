8d6f2e83b7a27c83050dec0eda3e7f4111b1753f
Boby <boby2014@abv.bg>
2022-10-26 17:15:47 +0300

    dashcam

---
+++ config.lua
@@ -0,0 +1,16 @@
+DashcamConfig = {}
+
+DashcamConfig.useMPH = false -- False will turn it to KMH	-- true
+
+DashcamConfig.RestrictVehicles = false	-- true
+DashcamConfig.RestrictionType = "class" -- custom / class
+
+DashcamConfig.AllowedVehicles = { -- LIST OF YOUR POLICE VEHICLES WHICH CAN USE DASHCAM AND BE RESTRICTED
+    "police",
+    "police1",
+    "police2",
+    "police3",
+    "police4",
+    "police5",
+    "police6",
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Dog#7087'
+description 'Dashcam for QBCore or Standalone'
+version '1.0.0'
+
+client_scripts {
+    "config.lua",
+    "client.lua"
+}
+
+ui_page "nui/index.html"
+files {
+    "nui/index.html",
+    "nui/vue.min.js",
+    "nui/script.js",
+    "nui/style.css",
+    "nui/images/seal.png"
+}
